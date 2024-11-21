a04ecb9d69c81ad4a581300b7ccf1dd44bc50032
Hritik Patel <151814714+hritikalways@users.noreply.github.com>
2024-11-21 17:39:25 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
   const handleSubmit = async () => {
   try {
     const payload = JSON.parse(jsonInput); // Validate JSON
-    const response = await axios.post("http://localhost:3001/api/bhfl", payload);
+    const response = await axios.post("https://bajaj-pi-ten.vercel.app/api/bhfl", payload);
 
     console.log("API Response:", response.data);
 
